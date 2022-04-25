79d09decf6cd2261e7229a1db3bf8b936e585eba
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-25 17:08:18 +1000

    Add names for foreground and background colours.

--- src/components/shared/Swatch.jsx
+++ src/components/shared/Swatch.jsx
@@ -1,13 +1,17 @@
 const Swatch = ({ paletteItem }) => {
+  const backgroundColorStyle = {
+    backgroundColor: 'grey'
+  };
+
   return (
-    <div
-      className="swatch"
-      style={{
-        backgroundColor: 'grey',
-        display: 'inline-block'
-      }}
-    >
+    <div className="swatch" style={backgroundColorStyle}>
       <div>{paletteItem.swatchName}</div>
+      <div>
+        <small>{paletteItem.foregroundColor}</small>
+      </div>
+      <div>
+        <small>{paletteItem.backgroundColor}</small>
+      </div>
     </div>
   );
 };
