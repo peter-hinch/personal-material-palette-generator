44abb6355e71a1998778b34ef0db4221452dc8d7
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-25 23:21:45 +1000

    Change default colors to hsl values. Add KeyColorOptions components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { useState } from 'react';
 import ColorScheme from './components/shared/ColorScheme';
+import KeyColorOptions from './components/shared/KeyColorOptions';
 import { ColorTheme } from './palette';
 
 function App() {
-  const [colorPrimary, setColorPrimary] = useState('#386a20');
-  const [colorSecondary, setColorSecondary] = useState('#55624c');
-  const [colorTertiary, setColorTertiary] = useState('#19686a');
-  const [colorNeutral, setColorNeutral] = useState('#5d5f5a');
-  const [colorNeutralVariant, setColorNeutralVariant] = useState('#5b6056');
-  const [colorError, setColorError] = useState('#b3261e');
+  const [colorPrimary, setColorPrimary] = useState({ h: 101, s: 54, l: 27 });
+  const [colorSecondary, setColorSecondary] = useState({ h: 95, s: 13, l: 34 });
+  const [colorTertiary, setColorTertiary] = useState({ h: 181, s: 62, l: 26 });
+  const [colorNeutral, setColorNeutral] = useState({ h: 84, s: 3, l: 36 });
+  const [colorNeutral2, setColorNeutral2] = useState({ h: 90, s: 5, l: 36 });
+  const [colorError, setColorError] = useState({ h: 3, s: 71, l: 41 });
 
   return (
     <>
       <form>
-        <input type="color" value={colorPrimary} />
-        <input type="color" value={colorSecondary} />
-        <input type="color" value={colorTertiary} />
-        <input type="color" value={colorNeutral} />
-        <input type="color" value={colorNeutralVariant} />
-        <input type="color" value={colorError} />
+        <KeyColorOptions name="Primary Key Colour" color={colorPrimary} />
+        <KeyColorOptions name="Secondary Key Colour" color={colorSecondary} />
+        <KeyColorOptions name="Tertiary Key Colour" color={colorTertiary} />
+        <KeyColorOptions name="Neutral Key Colour" color={colorNeutral} />
+        <KeyColorOptions
+          name="Neutral Variant Key Colour"
+          color={colorNeutral2}
+        />
+        <KeyColorOptions name="Error Key Colour" color={colorError} />
       </form>
       <ColorScheme colorTheme={ColorTheme.LIGHT} />
       <ColorScheme colorTheme={ColorTheme.DARK} />
