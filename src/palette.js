9e4e8bce15bc8cd4d03071d2bf549c929c3e3e1f
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-25 15:21:37 +1000

    Define light and dark colour schemes.

---
+++ src/palette.js
@@ -0,0 +1,240 @@
+export const Mode = {
+  LIGHT: 'light',
+  DARK: 'dark'
+};
+
+export const PaletteLight = {
+  PRIMARY: {
+    swatchName: 'Primary',
+    foregroundColor: 'primary-100',
+    backgroundColor: 'primary-40'
+  },
+  ON_PRIMARY: {
+    swatchName: 'On Primary',
+    foregroundColor: 'primary-40',
+    backgroundColor: 'primary-100'
+  },
+  PRIMARY_CONTAINER: {
+    swatchName: 'Primary Container',
+    foregroundColor: 'primary-10',
+    backgroundColor: 'primary-90'
+  },
+  ON_PRIMARY_CONTAINER: {
+    swatchName: 'On Primary Container',
+    foregroundColor: 'primary-90',
+    backgroundColor: 'primary-10'
+  },
+  SECONDARY: {
+    swatchName: 'Secondary',
+    foregroundColor: 'secondary-100',
+    backgroundColor: 'secondary-40'
+  },
+  ON_SECONDARY: {
+    swatchName: 'On Secondary',
+    foregroundColor: 'secondary-40',
+    backgroundColor: 'secondary-100'
+  },
+  SECONDARY_CONTAINER: {
+    swatchName: 'Secondary Container',
+    foregroundColor: 'secondary-10',
+    backgroundColor: 'secondary-90'
+  },
+  ON_SECONDARY_CONTAINER: {
+    swatchName: 'On Secondary Container',
+    foregroundColor: 'secondary-90',
+    backgroundColor: 'secondary-10'
+  },
+  TERTIARY: {
+    swatchName: 'Tertiary',
+    foregroundColor: 'tertiary-100',
+    backgroundColor: 'tertiary-40'
+  },
+  ON_TERTIARY: {
+    swatchName: 'On Tertiary',
+    foregroundColor: 'tertiary-40',
+    backgroundColor: 'tertiary-100'
+  },
+  TERTIARY_CONTAINER: {
+    swatchName: 'Tertiary Container',
+    foregroundColor: 'tertiary-10',
+    backgroundColor: 'tertiary-90'
+  },
+  ON_TERTIARY_CONTAINER: {
+    swatchName: 'On Tertiary Container',
+    foregroundColor: 'tertiary-90',
+    backgroundColor: 'tertiary-10'
+  },
+  ERROR: {
+    swatchName: 'Error',
+    foregroundColor: 'error-100',
+    backgroundColor: 'error-40'
+  },
+  ON_ERROR: {
+    swatchName: 'On Error',
+    foregroundColor: 'error-40',
+    backgroundColor: 'error-100'
+  },
+  ERROR_CONTAINER: {
+    swatchName: 'Error Container',
+    foregroundColor: 'error-10',
+    backgroundColor: 'error-90'
+  },
+  ON_ERROR_CONTAINER: {
+    swatchName: 'On Error Container',
+    foregroundColor: 'error-90',
+    backgroundColor: 'error-10'
+  },
+  BACKGROUND: {
+    swatchName: 'Background',
+    foregroundColor: 'neutral-10',
+    backgroundColor: 'neutral-99'
+  },
+  ON_BACKGROUND: {
+    swatchName: 'On Background',
+    foregroundColor: 'neutral-99',
+    backgroundColor: 'neutral-10'
+  },
+  SURFACE: {
+    swatchName: 'Surface',
+    foregroundColor: 'neutral-10',
+    backgroundColor: 'neutral-99'
+  },
+  ON_SURFACE: {
+    swatchName: 'On Surface',
+    foregroundColor: 'neutral-99',
+    backgroundColor: 'neutral-10'
+  },
+  SURFACE_VARIANT: {
+    swatchName: 'Surface Variant',
+    foregroundColor: 'neutral-variant-30',
+    backgroundColor: 'neutral-variant-90'
+  },
+  ON_SURFACE_VARIANT: {
+    swatchName: 'On Surface Variant',
+    foregroundColor: 'neutral-variant-90',
+    backgroundColor: 'neutral-variant-30'
+  },
+  OUTLINE: {
+    swatchName: 'Outline',
+    foregroundColor: 'neutral-variant-95',
+    backgroundColor: 'neutral-variant-50'
+  }
+};
+
+export const PaletteDark = {
+  PRIMARY: {
+    swatchName: 'Primary',
+    foregroundColor: 'primary-20',
+    backgroundColor: 'primary-80'
+  },
+  ON_PRIMARY: {
+    swatchName: 'On Primary',
+    foregroundColor: 'primary-80',
+    backgroundColor: 'primary-20'
+  },
+  PRIMARY_CONTAINER: {
+    swatchName: 'Primary Container',
+    foregroundColor: 'primary-90',
+    backgroundColor: 'primary-30'
+  },
+  ON_PRIMARY_CONTAINER: {
+    swatchName: 'On Primary Container',
+    foregroundColor: 'primary-30',
+    backgroundColor: 'primary-90'
+  },
+  SECONDARY: {
+    swatchName: 'Secondary',
+    foregroundColor: 'secondary-20',
+    backgroundColor: 'secondary-80'
+  },
+  ON_SECONDARY: {
+    swatchName: 'On Secondary',
+    foregroundColor: 'secondary-80',
+    backgroundColor: 'secondary-20'
+  },
+  SECONDARY_CONTAINER: {
+    swatchName: 'Secondary Container',
+    foregroundColor: 'secondary-90',
+    backgroundColor: 'secondary-30'
+  },
+  ON_SECONDARY_CONTAINER: {
+    swatchName: 'On Secondary Container',
+    foregroundColor: 'secondary-30',
+    backgroundColor: 'secondary-90'
+  },
+  TERTIARY: {
+    swatchName: 'Tertiary',
+    foregroundColor: 'tertiary-20',
+    backgroundColor: 'tertiary-80'
+  },
+  ON_TERTIARY: {
+    swatchName: 'On Tertiary',
+    foregroundColor: 'tertiary-80',
+    backgroundColor: 'tertiary-20'
+  },
+  TERTIARY_CONTAINER: {
+    swatchName: 'Tertiary Container',
+    foregroundColor: 'tertiary-90',
+    backgroundColor: 'tertiary-30'
+  },
+  ON_TERTIARY_CONTAINER: {
+    swatchName: 'On Tertiary Container',
+    foregroundColor: 'tertiary-30',
+    backgroundColor: 'tertiary-90'
+  },
+  ERROR: {
+    swatchName: 'Error',
+    foregroundColor: 'error-100',
+    backgroundColor: 'error-40'
+  },
+  ON_ERROR: {
+    swatchName: 'On Error',
+    foregroundColor: 'error-40',
+    backgroundColor: 'error-100'
+  },
+  ERROR_CONTAINER: {
+    swatchName: 'Error Container',
+    foregroundColor: 'error-10',
+    backgroundColor: 'error-90'
+  },
+  ON_ERROR_CONTAINER: {
+    swatchName: 'On Error Container',
+    foregroundColor: 'error-90',
+    backgroundColor: 'error-10'
+  },
+  BACKGROUND: {
+    swatchName: 'Background',
+    foregroundColor: 'neutral-90',
+    backgroundColor: 'neutral-10'
+  },
+  ON_BACKGROUND: {
+    swatchName: 'On Background',
+    foregroundColor: 'neutral-10',
+    backgroundColor: 'neutral-90'
+  },
+  SURFACE: {
+    swatchName: 'Surface',
+    foregroundColor: 'neutral-90',
+    backgroundColor: 'neutral-10'
+  },
+  ON_SURFACE: {
+    swatchName: 'On Surface',
+    foregroundColor: 'neutral-10',
+    backgroundColor: 'neutral-90'
+  },
+  SURFACE_VARIANT: {
+    swatchName: 'Surface Variant',
+    foregroundColor: 'neutral-variant-80',
+    backgroundColor: 'neutral-variant-30'
+  },
+  ON_SURFACE_VARIANT: {
+    swatchName: 'On Surface Variant',
+    foregroundColor: 'neutral-variant-30',
+    backgroundColor: 'neutral-variant-80'
+  },
+  OUTLINE: {
+    swatchName: 'Outline',
+    foregroundColor: 'neutral-variant-20',
+    backgroundColor: 'neutral-variant-60'
+  }
+};
