f1e97892ba537e5cdc43f0f7022d77ed4646e17f
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-25 17:06:50 +1000

    Add useState hook and inputs for default colours.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
-import ColorScheme from './components/ColorScheme';
+import { useState } from 'react';
+import ColorScheme from './components/shared/ColorScheme';
 import { ColorTheme } from './palette';
 
 function App() {
+  const [colorPrimary, setColorPrimary] = useState('#386a20');
+  const [colorSecondary, setColorSecondary] = useState('#55624c');
+  const [colorTertiary, setColorTertiary] = useState('#19686a');
+  const [colorNeutral, setColorNeutral] = useState('#5d5f5a');
+  const [colorNeutralVariant, setColorNeutralVariant] = useState('#5b6056');
+  const [colorError, setColorError] = useState('#b3261e');
+
   return (
     <>
       <form>
-        <input type="color" />
+        <input type="color" value={colorPrimary} />
+        <input type="color" value={colorSecondary} />
+        <input type="color" value={colorTertiary} />
+        <input type="color" value={colorNeutral} />
+        <input type="color" value={colorNeutralVariant} />
+        <input type="color" value={colorError} />
       </form>
       <ColorScheme colorTheme={ColorTheme.LIGHT} />
       <ColorScheme colorTheme={ColorTheme.DARK} />
