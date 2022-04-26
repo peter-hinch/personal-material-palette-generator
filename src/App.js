25e45a72b838ffc5c48013d4c42887671735a56d
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-26 12:26:48 +1000

    Refactor separate color states into one nested object.

--- src/App.js
+++ src/App.js
@@ -4,32 +4,34 @@ import KeyColorOptions from './components/shared/KeyColorOptions';
 import { ColorTheme } from './palette';
 
 function App() {
-  const [colorPrimary, setColorPrimary] = useState({ h: 101, s: 54, l: 27 });
-  const [colorSecondary, setColorSecondary] = useState({ h: 95, s: 13, l: 34 });
-  const [colorTertiary, setColorTertiary] = useState({ h: 181, s: 62, l: 26 });
-  const [colorNeutral, setColorNeutral] = useState({ h: 84, s: 3, l: 36 });
-  const [colorNeutral2, setColorNeutral2] = useState({ h: 90, s: 5, l: 36 });
-  const [colorError, setColorError] = useState({ h: 3, s: 71, l: 41 });
+  const [colors, setColors] = useState({
+    primary: { h: 101, s: 54, l: 27 },
+    secondary: { h: 95, s: 13, l: 34 },
+    tertiary: { h: 181, s: 62, l: 26 },
+    neutral: { h: 84, s: 3, l: 36 },
+    neutralVariant: { h: 90, s: 5, l: 36 },
+    error: { h: 3, s: 71, l: 41 }
+  });
 
   return (
     <>
       <h1>Material Design 3 Color Palette Generator</h1>
       <form>
         <h2>Accent Colours</h2>
-        <KeyColorOptions name="Primary Key Colour" color={colorPrimary} />
-        <KeyColorOptions name="Secondary Key Colour" color={colorSecondary} />
-        <KeyColorOptions name="Tertiary Key Colour" color={colorTertiary} />
+        <KeyColorOptions name="Primary Key Colour" color={colors.primary} />
+        <KeyColorOptions name="Secondary Key Colour" color={colors.secondary} />
+        <KeyColorOptions name="Tertiary Key Colour" color={colors.tertiary} />
         <h2>Neutral Colours</h2>
-        <KeyColorOptions name="Neutral Key Colour" color={colorNeutral} />
+        <KeyColorOptions name="Neutral Key Colour" color={colors.neutral} />
         <KeyColorOptions
           name="Neutral Variant Key Colour"
-          color={colorNeutral2}
+          color={colors.neutralVariant}
         />
         <h2>Additional Colours</h2>
-        <KeyColorOptions name="Error Key Colour" color={colorError} />
+        <KeyColorOptions name="Error Key Colour" color={colors.error} />
       </form>
-      <ColorScheme colorTheme={ColorTheme.LIGHT} />
-      <ColorScheme colorTheme={ColorTheme.DARK} />
+      <ColorScheme colors={colors} colorTheme={ColorTheme.LIGHT} />
+      <ColorScheme colors={colors} colorTheme={ColorTheme.DARK} />
     </>
   );
 }
