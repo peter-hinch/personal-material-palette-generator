57bbc713646150b360aaef0be7a14ec1a7ee6c22
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-26 12:55:42 +1000

    Pass colors state to Swatch component.

--- src/components/shared/ColorScheme.jsx
+++ src/components/shared/ColorScheme.jsx
@@ -1,7 +1,7 @@
 import Swatch from './Swatch';
 import { ColorTheme, PaletteLight, PaletteDark } from '../../palette';
 
-const ColorScheme = ({ colorTheme }) => {
+const ColorScheme = ({ colors, colorTheme }) => {
   let scheme;
   if (colorTheme === ColorTheme.LIGHT) {
     scheme = PaletteLight;
@@ -10,7 +10,9 @@ const ColorScheme = ({ colorTheme }) => {
   }
 
   const renderPaletteItems = Object.entries(scheme).map(
-    ([key, paletteItem]) => <Swatch key={key} paletteItem={paletteItem} />
+    ([key, paletteItem]) => (
+      <Swatch key={key} colors={colors} paletteItem={paletteItem} />
+    )
   );
 
   return <div className="color-scheme">{renderPaletteItems}</div>;
